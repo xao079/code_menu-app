a73d0ece0c76656ebbae01b03089d7257c08472f
Soumyakumar07 <raegon07xao@gmail.com>
2025-06-27 13:35:02 +0530

    Add files via upload

---
+++ MARKET ANALYSIS/Trading.py
@@ -0,0 +1,31 @@
+import streamlit as st 
+
+st.set_page_config( 
+    page_title= "GlobalTradeHub",
+    page_icon ="chart_with_downwards_trends:",
+    layout = "wide"
+
+)
+
+st.title("Global Trade Hub :bar_chart")
+
+st.header("We will Find Your way, So you don't have to Look for a Shelter. ")
+
+st.image("bar.png")
+
+st.markdown("## WE are hoping for the bright future. ")
+
+st.markdown("#### :one: Stock Information")
+st.write("Through this page, you can see all the information about the stock. ")
+
+st.markdown("#### :two: Stock iNFO. ")
+st.write("You can explore and find your interest and invest. ")
+
+st.markdown("#### :three: Stock Analysis")
+st.write("Explore and Analyze the stock and their benifits.")
+
+st.markdown("#### :four: CAPM Return. ")
+st.write("Discover how the Capital Asset Pricing Model (CAPM) calculates the expected return o different stocks asset bassed on its risk and return.")
+
+st.markdown("#### :five: CAPM Beta. " )
+st.write("Calculate Beta and Expected Return for Individual Stocks. ")
